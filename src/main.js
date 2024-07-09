81d8f5780893de2c1afeae9586009474253878b1
Esneyder Galviz <119534032+gitgalviz@users.noreply.github.com>
2024-07-08 21:59:17 -0500

    Codigo base, librerias y demas, completo

--- src/main.js
+++ src/main.js
@@ -1,5 +1,21 @@
 import { createApp } from 'vue'
 import './style.css'
+import { router } from './routes/routes.js'
+import { createPinia } from 'pinia'
+import  piniaPluginPersistedState from 'pinia-plugin-persistedstate'
+import { Quasar } from 'quasar'
+import '@quasar/extras/material-icons/material-icons.css'
+import 'quasar/src/css/index.sass'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedState)
+const app = createApp(App)
+
+app.use(router)
+app.use(pinia)
+app.use(Quasar)
+
+
+app.mount('#app')
+
